3a8a880775a51bb00e930f34d7c7904c9fb96b26
alexz12948 <alesandor00@gmail.com>
2019-09-23 14:14:28 -0400

    Reads sample file and throws exception website

--- web_scrape.py
+++ web_scrape.py
@@ -1,10 +1,45 @@
 from bs4 import BeautifulSoup
 import requests
+import sys
+
+def remove_newline(s):
+    for i in range(len(s)):
+        if s[i] == '\n':
+            del s[i]
+    
+    print(s)
+
+def count_vowels(s):
+    pass
+
+if len(sys.argv) != 2:
+    print("Usage: python3 ./web_scrape.py <URL>")
+    exit(1)
+
+try:
+    source = requests.get(sys.argv[1]).text
+except Exception as e:
+    print(f"Error: {e}")
+    #exit(1) 
 
 with open('simple.html') as html_file:
-    soup = BeautifulSoup(html_file, 'lxml')
+    soup = BeautifulSoup(html_file, 'lxml') 
 
-print(soup.prettify())
-print(soup.title.text)
-print(soup.find('div', class_='footer').text)
+text = soup.get_text().split('\n')
+
+remove_newline(text)
+
+for word in text:
+    if word == '':
+        continue
+    else:
+        print(f"Word: {word}")
+
+
+
+
+
+#print(soup.prettify())
+#print(soup.title.text)
+#print(soup.find('div', class_='footer').text)
 
