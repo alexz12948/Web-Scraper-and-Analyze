9c57c28f9547e337804c008f94be509af7846b85
alexz12948 <alesandor00@gmail.com>
2019-09-23 15:40:20 -0400

    Frequency Array is finished

--- web_scrape.py
+++ web_scrape.py
@@ -12,24 +12,49 @@ import requests
 import sys
 
 '''
-Input:
-Output:
-Does:
+Input: a list of lines from an html file
+Output: N/A
+Does: Removes the new line character form the list
 '''
 def remove_newline(s):
     for i in range(len(s)):
         if s[i] == '\n':
             del s[i]
-    
-    print(s)
 
 '''
-Input:
-Output:
-Does:
+Input: a list of text
+Output: a frequency list of letters
+Does: counts the number of letters in an html file
 '''
-def count_vowels(s):
-    pass
+def count_letters(text):
+    freq = [0] * 26
+
+    for string in text:
+        for char in string:
+            if not char.isalpha():
+                break
+            else:
+                freq[letter_index(char)] += 1
+
+    return freq
+
+'''
+Input: a character
+Output: the index of that character
+Does: returns the location in the freq array where the character 
+      should be put
+'''
+def letter_index(c):
+    return ord(c.upper()) % 65
+
+'''
+Input: a frequency list of characters
+Output: N/A
+Does: prints out the frequency of each character to the user
+'''
+def print_freq(freq):
+    for i in range(len(freq)):
+        print(f"{chr(i + 65)} is in the file {freq[i]} time(s)")
 
 if len(sys.argv) != 2:
     print("Usage: python3 ./web_scrape.py <URL>")
@@ -48,17 +73,8 @@ text = soup.get_text().split('\n')
 
 remove_newline(text)
 
-for word in text:
-    if word == '':
-        continue
-    else:
-        print(f"Word: {word}")
+freq = count_letters(text)
 
+print_freq(freq)
 
-
-
-
-#print(soup.prettify())
-#print(soup.title.text)
-#print(soup.find('div', class_='footer').text)
-
+exit(0)
